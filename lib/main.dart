390312be59fec33dc7469d350ce4c360b2ae63bf
Kalpu-24 <22amtics013@gmail.com>
2024-10-14 21:48:59 +0530

    adjusted icon, dynamic color, responsive scaffold

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,143 @@
+import 'package:dynamic_color/dynamic_color.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:fusix/prefs/theme_mode_manager.dart';
+import 'package:provider/provider.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(ChangeNotifierProvider(
+    create: (context) => ThemeModeManager(),
+    child: FusixApp(),
+  ));
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class FusixApp extends StatelessWidget {
+  const FusixApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+    return ScreenUtilInit(
+      designSize: const Size(360, 690), // Base design size
+      minTextAdapt: true, // Responsive text adaptation
+      builder: (context, child) {
+        return DynamicColorBuilder(
+          builder: (ColorScheme? lightDynamic, ColorScheme? darkDynamic) {
+            ColorScheme? lightScheme;
+            ColorScheme? darkScheme;
+
+            if (!kIsWeb) {
+              lightScheme = lightDynamic;
+              darkScheme = darkDynamic;
+            } else {
+              lightScheme = ColorScheme.fromSeed(seedColor: Colors.blue);
+              darkScheme = ColorScheme.fromSeed(
+                seedColor: Colors.blue,
+                brightness: Brightness.dark,
+              );
+            }
+
+            return MaterialApp(
+              theme: ThemeData(colorScheme: lightScheme, useMaterial3: true),
+              darkTheme: ThemeData(colorScheme: darkScheme, useMaterial3: true),
+              themeMode: ThemeMode.system,
+              home: const ResponsiveLayout(),
+            );
+          },
+        );
+      },
+    );
+  }
+}
+
+class ResponsiveLayout extends StatefulWidget {
+  const ResponsiveLayout({super.key});
+
+  @override
+  ResponsiveLayoutState createState() => ResponsiveLayoutState();
+}
+
+class ResponsiveLayoutState extends State<ResponsiveLayout> {
+  int _selectedIndex = 0;
+
+  final List<Widget> _pages = [
+    const Center(child: Text('Home Page')),
+    const Center(child: Text('Library')),
+    const Center(child: Text('Settings')),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    bool isWideScreen = MediaQuery.of(context).size.width > 800;
+    debugPrint(
+        'isWideScreen: $isWideScreen, width: ${MediaQuery.of(context).size.width}');
+
+    return Scaffold(
+      body: Row(
+        children: [
+          if (isWideScreen) ...[
+            NavigationDrawer(
+              selectedIndex: _selectedIndex,
+              onDestinationSelected: (int index) {
+                setState(() {
+                  _selectedIndex = index;
+                });
+              },
+              backgroundColor: Theme.of(context).colorScheme.surface,
+              children: const [
+                NavigationDrawerDestination(
+                  icon: Icon(Icons.home_outlined),
+                  selectedIcon: Icon(Icons.home),
+                  label: Text('Home'),
+                ),
+                SizedBox(height: 8),
+                NavigationDrawerDestination(
+                  icon: Icon(Icons.library_music_outlined),
+                  selectedIcon: Icon(Icons.library_music),
+                  label: Text('Library'),
+                ),
+                SizedBox(height: 8),
+                NavigationDrawerDestination(
+                  icon: Icon(Icons.settings_outlined),
+                  selectedIcon: Icon(Icons.settings),
+                  label: Text('Settings'),
+                ),
+              ],
+            ),
+          ],
+          Expanded(
+            child: _pages[_selectedIndex],
+          ),
+        ],
       ),
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
+      bottomNavigationBar: isWideScreen
+          ? null
+          : NavigationBar(
+              selectedIndex: _selectedIndex,
+              onDestinationSelected: (int index) {
+                setState(() {
+                  _selectedIndex = index;
+                });
+              },
+              backgroundColor: Theme.of(context).colorScheme.surface,
+              destinations: const [
+                NavigationDestination(
+                  icon: Icon(Icons.home_outlined),
+                  selectedIcon: Icon(Icons.home),
+                  label: 'Home',
+                ),
+                NavigationDestination(
+                  icon: Icon(Icons.library_music_outlined),
+                  selectedIcon: Icon(Icons.library_music),
+                  label: 'Library',
+                ),
+                NavigationDestination(
+                  icon: Icon(Icons.settings_outlined),
+                  selectedIcon: Icon(Icons.settings),
+                  label: 'Settings',
+                ),
+              ],
+            ),
     );
   }
 }
